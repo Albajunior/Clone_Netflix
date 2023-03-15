3c92d013dadeee93e0ccc678d441b71c1255eed9
Albajunior <guissealba@gmail.com>
2023-03-15 21:56:02 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php include("./ui/header.php"); ?>
+
+<div class="container p-5">
+    <div class="m-auto border border-white d-flex align-items-center justify-content-center flex-column w-75">
+        <h1 class="text-white">Ma plateforme</h1>
+        <img class="logo" src="./images/Netflix-logo.png" alt="" style="width: 300px; height: 150px;">
+    </div>
+
+        
+    <div class="imgtop">
+    <img src="images/netflix.jpg" style=" height: 500px;   
+    margin-left: auto;
+    justify-content: center;
+    display: flex;
+    align-items: center;
+    margin-right: auto; "  alt="tofSerie" class="imgtop"></div>
+
+
+</div>
+
+<?php include("./ui/footer.php"); ?>
